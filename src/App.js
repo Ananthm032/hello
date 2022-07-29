0a08316d32db2494678ed57748d8cab401a6b2d0
Ananthm032 <ananthm6092@gmail.com>
2022-07-29 16:26:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,29 +2,24 @@ import { useContext } from 'react'
 import Rejected from './Rejected/Rejected';
 import Verified from "./Verified/Verified"
 import { pagesMapping, RoutingContext } from './Context/Routing'
-import Anal from "./Anal/Anal"
 import './App.css';
-
 import Forreview from './Forreview/Forreview';
 import { Route, Routes } from 'react-router-dom';
 import Ananlytic from './Ananlaytic/Ananlytic';
-
 function App() {
   const { page } = useContext(RoutingContext)
 
   return (
     <>
-     <div>
+    
       {(pagesMapping.sidebar === page) && <Forreview />}
       {(pagesMapping.largeside === page) && <Verified />}
       {(pagesMapping.mini=== page) && <Rejected />}
-      </div>
-      <div>
-      <Routes>
-        <Route path="/" element={<Forreview/>}/>
-       <Route path='/analytic' element={<Ananlytic/>}></Route>
-     </Routes>
-     </div>
+
+   <Routes>   <Route path='/analytic' element={ <Ananlytic/>}>
+       
+      </Route></Routes>
+    
     </>
    
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import Router from './Context/Routing'
 import {BrowserRouter} from "react-router-dom"
