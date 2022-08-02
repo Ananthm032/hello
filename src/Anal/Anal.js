e89bc684235ffab8ebf36a52951aee73b86cd665
Ananthm032 <ananthm6092@gmail.com>
2022-08-02 15:55:29 +0530

    first commit

--- src/Anal/Anal.js
+++ src/Anal/Anal.js
@@ -1,26 +1,26 @@
 
 
-import { Box } from '@mui/material'
+import Img3 from "../Images/navbar.png"
 import ArticleIcon from '@mui/icons-material/Article'; 
 import StopIcon from '@mui/icons-material/Stop';
-
-import {Link} from "react-router-dom"
 import "./anal.css"
 function Anal() {
  
   return (
     <div className='contain'>
         <section class="anal">
-        <div class="dropdown">
-  <button class="dropbtn"><ArticleIcon />Creater's Application</button>
-  <div class="dropdown-content">
+        <div class="drop">
+          <img  className='name'src={Img3} alt=''/>
+  <button><ArticleIcon />Creater's Application</button>
+  <div class="drop-content">
+  <p><ArticleIcon />Creater's Application</p>
   <a href="/"><StopIcon/>Home</a>
   <a href="/analytic"><StopIcon/>Analytic</a>
  
   </div>
 </div>
     </section>
-    <section>
+    <section className="tic">
 
         <h1>Ananlaytic</h1>
     </section>
